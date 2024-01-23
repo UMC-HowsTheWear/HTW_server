0df3d4c87a9c51e87e9a9d6bb6f703674d4c2ee8
snowgaram <tlwns0401@naver.com>
2024-01-23 13:05:04 +0900

    첫 코드 작성

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import { tempRouter } from './routes/temp.route.js';
 import { response } from './config/response.js';
-import { userRouter } from './routes/user.route.js';
 import { specs } from './swagger/swagger.config.js';
 import SwaggerUi from 'swagger-ui-express';
+
+import { userRouter } from './src/routes/user.route.js';
 
 dotenv.config();
 
@@ -13,19 +13,22 @@ const app = express();
 
 app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
 
-// swagger
-app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
-
-// router setting
-app.use('/temp', tempRouter);
-app.use('/user', userRouter);
-
 // server setting - veiw, static, body-parser etc..
 app.set('port', process.env.PORT || 3000)   // 서버 포트 지정
 app.use(cors());                            // cors 방식 허용
 app.use(express.static('public'));          // 정적 파일 접근
 app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
+
+app.get('/', (req, res, next) => {
+    res.send(response(status.SUCCESS, "루트 페이지!"));
+})
+
+// swagger
+app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
+
+// router setting
+app.use('/user', userRouter);
 
 app.use((err, req, res, next) => {
     // 템플릿 엔진 변수 설정
