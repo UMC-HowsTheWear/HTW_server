ddd7e61b9d5a0f8b0b783e29ba1dff7ce823c7b2
snowgaram <tlwns0401@naver.com>
2024-01-25 01:16:25 +0900

    test용 2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import SwaggerUi from 'swagger-ui-express';
 
 import { userRouter } from './src/routes/user.route.js';
 import { aroundRouter } from './src/routes/around.route.js';
+import { postRouter } from './src/routes/post.route.js';
 
 dotenv.config();
 
@@ -31,6 +32,7 @@ app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(specs));
 // router setting
 app.use('/user', userRouter);
 app.use('/user/around',aroundRouter);
+app.use('/user/:id', postRouter);
 
 app.use((err, req, res, next) => {
     // 템플릿 엔진 변수 설정
@@ -40,12 +42,17 @@ app.use((err, req, res, next) => {
     console.log("error", err);
     res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
 });
-            
-
-////시범용
-//안녕하세용
-//한번더
 
 app.listen(app.get('port'), () => {
     console.log(`Example app listening on port ${app.get('port')}`);
 });
+
+
+
+
+
+
+
+////시범용
+//안녕하세용
+//한번더
