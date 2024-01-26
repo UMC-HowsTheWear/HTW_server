c493af564c9790f1065c9c5249eb9b86045db3ca
Chajeongeun <thfhd11@naver.com>
2024-01-26 16:00:38 +0900

    RDB 연결전 mia 브랜치로 보내기

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,21 +8,28 @@ import { joinUser, loginUser } from './../services/user.service.js';
 
 // 회원가입
 export const userJoin = async (req, res, next) => {
-    const signIn = req.body; //req.body값을 signIn에 넣음.
+    const signIn = req.body;
     console.log("회원가입을 요청하였습니다!");
-    console.log("body:", signIn); // 위의 값이 잘 들어오는지 테스트
+    console.log("body:", signIn);
 
-    const signIndata = await joinUser(req.body); //함수의 반환값 signindata에 넣음
-    
-    if(signIndata == -1){ //반환값이 -1이라면
-        console.log("아이디가 중복 됩니다.");
-        return res.status(StatusCodes.BAD_REQUEST).json({ //응답 상태코드를 400준다
-            message: "회원가입 실패",
-          }); 
-    } else {
-        console.log("회원 가입 성공");
-        return res.status(StatusCodes.OK).json({ //상태코드 200(ok)와 성공메세지 출력
-            message: "회원가입 성공",
+    try {
+        const signIndata = await joinUser(req.body);
+        
+        if (signIndata === null) { // 중복된 아이디가 있을 때
+            console.log("아이디가 중복됩니다.");
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: "아이디가 중복됩니다.",
+            }); 
+        } else { // 회원가입 성공
+            console.log("회원 가입 성공");
+            return res.status(StatusCodes.OK).json({
+                message: "회원가입 성공",
+            });
+        }
+    } catch (error) {
+        console.log("회원가입 실패");
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "서버 에러, 관리자에게 문의 바랍니다.",
         });
     }
 }
