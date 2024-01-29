7182d5fef732bcb022f765adfaa5b7d3894f7250
snowgaram <tlwns0401@naver.com>
2024-01-29 10:03:19 +0900

    do to pull

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,11 +1,80 @@
 import { BaseError } from "../../config/error.js";
 import { status } from "../../config/response.status.js";
-import {  } from "../dtos/post.dto.js"
+import { getPostResponseDTO, postPostResponseDTO } from "../dtos/post.dto.js"
+import { patchPostResponseDTO, deletePostResponseDTO } from "../dtos/post.dto.js"
+import { getPageResponseDTO, patchPageResponseDTO } from "../dtos/post.dto.js"
+import { addArticle, getArticle } from "../models/post.dao.js";
 
-export const getpostData = async (body) => {
-    const prefer = body.prefer;
+export const getPost = async (body) => {
+    const getPostData = await getArticle({
+        'post_id': body.id,
+        'user_id': body.user_id,
+        'name': body.name,
+        'temperature': body.temperature,
+        'item': body.item,
+        'location': body.location
+    });
 
-    const joinUserData = await addUser({
+    if(getPostData == -1){
+        throw new BaseError(status.POST_ALREADY_EXIST);
+    }
+
+    return getPostResponseDTO(await getUser(joinUserData));
+}
+
+export const postPost = async (body) => {
+    const postPostData = await addArticle({
+        'post_id': body.id,
+        'user_id': body.user_id,
+        'name': body.name,
+        'temperature': body.temperature,
+        'item': body.item,
+        'location': body.location
+    });
+
+    if(postPostData == -1){
+        throw new BaseError(status.POST_ALREADY_EXIST);
+    }
+
+    return postPostResponseDTO(await getArticle(joinUserData), await getUserPreferToUserID(joinUserData));
+}
+
+export const patchPost = async (body) => {
+    const patchPostData = await addUser({
+        'post_id': body.id,
+        'user_id': body.user_id,
+        'name': body.name,
+        'temperature': body.temperature,
+        'item': body.item,
+        'location': body.location
+    });
+
+    if(patchPostData == -1){
+        throw new BaseError(status.EMAIL_ALREADY_EXIST);
+    }
+
+    return patchPostResponseDTO(await getUser(joinUserData), await getUserPreferToUserID(joinUserData));
+}
+
+export const deletePost = async (body) => {
+    const deletePostData = await addUser({
+        'post_id': body.id,
+        'user_id': body.user_id,
+        'name': body.name,
+        'temperature': body.temperature,
+        'item': body.item,
+        'location': body.location
+    });
+
+    if(deletePostData == -1){
+        throw new BaseError(status.EMAIL_ALREADY_EXIST);
+    }
+
+    return deletePostResponseDTO(await getUser(joinUserData), await getUserPreferToUserID(joinUserData));
+}
+
+export const getPage = async (body) => {
+    const getPageData = await addUser({
         'email': body.email,
         'name': body.name,
         'gender': body.gender,
@@ -17,32 +86,29 @@ export const getpostData = async (body) => {
         'phone': body.phone
     });
 
-    if(joinUserData == -1){
+    if(getPageData == -1){
         throw new BaseError(status.EMAIL_ALREADY_EXIST);
-    }else{
-        for (let i = 0; i < prefer.length; i++) {
-            await setPrefer(joinUserData, prefer[i]);
-        }
-        return signinResponseDTO(await getUser(joinUserData), await getUserPreferToUserID(joinUserData));
     }
+
+    return getPageResponseDTO(await getUser(joinUserData), await getUserPreferToUserID(joinUserData));
 }
 
-export const postpostData = async (body) => {
+export const patchPage = async (body) => {
+    const patchPageData = await addUser({
+        'email': body.email,
+        'name': body.name,
+        'gender': body.gender,
+        'birthYear': body.birthYear,
+        'birthMonth': body.birthMonth,
+        'birthDay': body.birthDay,
+        'addr': body.addr,
+        'specAddr': body.specAddr,
+        'phone': body.phone
+    });
 
-}
+    if(patchPageData == -1){
+        throw new BaseError(status.EMAIL_ALREADY_EXIST);
+    }
 
-export const patchpostData = async (body) => {
-
-}
-
-export const deletepostData = async (body) => {
-
-}
-
-export const getpageData = async (body) => {
-
-}
-
-export const patchpageData = async (body) => {
-
+    return patchPageResponseDTO(await getUser(joinUserData), await getUserPreferToUserID(joinUserData));
 }
